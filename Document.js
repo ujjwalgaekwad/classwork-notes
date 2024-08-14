bb68c50743ae016e7fef642613a2e62b3f45699d
Ujjwal Singh <165365859+ujjwalgaikwad@users.noreply.github.com>
2024-08-14 22:01:02 +0530

    Update Document.js

--- Document.js
+++ Document.js
@@ -2,15 +2,20 @@ let newbtn = document.createElement("button");
 newbtn.style.backgroundColor = "blue";
 newbtn.style.color = "#fff";
 newbtn.innerText ="Click me!";
-document.querySelector("body").prepend(newbtn);  // top align
+document.querySelector("body").prepend(newbtn);  // top 
 
 let paragraph = document.createElement("p");
 paragraph.innerText ="this is div";
-document.querySelector("body").append(paragraph);//bottom align
+document.querySelector("body").append(paragraph);//bottom 
 
 let headingTag = document.createElement("h1");
 headingTag.style.textAlign ="center";
 headingTag.style.color = "yellow";
 headingTag.style.fontWeight = "800";
 headingTag.innerText ="Hello guys!";
-document.querySelector("body").after(headingTag);//bottom align
+document.querySelector("body").after(headingTag);//bottom 
+
+//class list 
+let heading= document.querySelector("h1");
+heading.classList.add("degree"); //add another class inside class 
+heading.classList.remove("degree"); //Remove class
