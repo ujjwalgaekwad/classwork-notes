6e83e5c150aaf288fb63f37f96b001bd7bda1649
Ujjwal Singh <165365859+ujjwalgaikwad@users.noreply.github.com>
2024-09-10 18:33:01 +0530

    Add files via upload

---
+++ Java/FirstClass.java
@@ -0,0 +1,30 @@
+class FirstClass {
+    public static void main(String args[]) {
+        // First Program
+        System.out.println("hello ujjwal");
+
+        //variables
+        int age = 17;
+        System.out.println(age);
+        
+        // Data types 1) Primitive 2)Non-primitive 
+
+        //byte - 1 [-128 to 127]
+        //short - 2 
+        //int - 4 
+        //long - 8
+        //float - 4 , 3.14
+        //double - 8 
+        //char - 2 a,b,c,d 
+        //boolean - 1 true/false
+
+        //Strings
+        //Concatenate
+        String firstName = "ujjwal";
+        String lastName = "Singh";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+        System.out.println(fullName.charAt(0));
+        System.out.println(fullName.length());
+    }
+}
